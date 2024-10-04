4399f7f520617dfe853afe25d375ea7cefd62638
AlejandroRivas <a.rivaslopez020@gmail.com>
2024-10-03 20:48:44 -0500

    docs(book): add documentation for book module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Library API')
+    .setDescription('API for managing books')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+  Logger.log(`app is running at http://localhost:${port}`);
+  Logger.log(
+    `swagger documentation available at http://localhost:${port}/api/docs`,
+  );
 }
 bootstrap();
